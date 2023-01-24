0412f1907795b9744775f7e205ba94abe651c215
H Y S T E R 1 A <97292612+Hyster1a20@users.noreply.github.com>
2023-01-24 13:31:44 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+import os
+import telebot
+
+tk = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(tk)
+
+@bot.message_handler(commands=['start', 'alo'])
+def send_welcome(message):
+    bot.reply_to(message, "hector salamanca beesh")
